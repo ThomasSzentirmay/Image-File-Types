3e0abc0c14dc36535ed8f6efab7807d57162ec7f
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-11 18:39:58 -0400

    made get request to send the user index.html

--- server.js
+++ server.js
@@ -2,3 +2,12 @@ const express = require('express');
 const multer = require('multer');
 const sharp = require('sharp');
 const path = require('path');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
