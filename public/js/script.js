9495c85ca3117ff67642d6c3df6edf06f75b115d
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-11 20:01:13 -0400

    using filepond package to see if its a package bug

--- public/js/script.js
+++ public/js/script.js
@@ -1,65 +1,48 @@
 window.onload = function () {
-  const convertBtn = document.querySelector("#convertBtn");
-  if (convertBtn) {
-    convertBtn.addEventListener("click", convertImage);
-  }
+  // Initialize FilePond instance
+  const imageFileInput = document.querySelector("#imageFile");
+  const pond = FilePond.create(imageFileInput);
 
+  // Get other elements
+  const convertBtn = document.querySelector("#convertBtn");
+  const formatSelect = document.querySelector("#formatSelect");
+
+  // Convert button click event handler
+  convertBtn.addEventListener("click", function () {
+    const file = pond.getFile();
+    if (!file) {
+      alert("Please select an image file.");
+      return;
+    }
+
+    const format = formatSelect.value;
+
+    file.convert(
+      "image/png",
+      { type: `image/${format}`, quality: 0.8 },
+      (convertedFile) => {
+        const convertedImageData = URL.createObjectURL(convertedFile);
+        const outputImage = document.querySelector("#outputImage");
+        if (outputImage) {
+          outputImage.src = convertedImageData;
+        }
+      }
+    );
+  });
+
+  // Download button click event handler
   const downloadBtn = document.querySelector("#downloadBtn");
   if (downloadBtn) {
-    downloadBtn.addEventListener("click", downloadImage);
+    downloadBtn.addEventListener("click", function () {
+      const outputImage = document.querySelector("#outputImage");
+      if (!outputImage) {
+        return;
+      }
+
+      const link = document.createElement("a");
+      link.href = outputImage.src;
+      link.download = "converted_image.jpg";
+      link.click();
+    });
   }
 };
-
-function convertImage() {
-  const fileInput = document.querySelector("#imageFile");
-  const file = fileInput.files[0];
-
-  if (!file) {
-    alert("Please select an image file.");
-    return;
-  }
-
-  const formatSelect = document.querySelector("#formatSelect");
-  const format = formatSelect.value;
-
-  const reader = new FileReader();
-  reader.onload = function (event) {
-    const img = new Image();
-    img.onload = function () {
-      const canvas = document.createElement("canvas");
-      canvas.width = img.width;
-      canvas.height = img.height;
-      const context = canvas.getContext("2d");
-      context.drawImage(img, 0, 0);
-
-      const convertedImageData = canvas.toDataURL(`image/${format}`);
-      const outputImage = document.querySelector("#outputImage");
-
-      if (outputImage) {
-        outputImage.src = convertedImageData;
-      } else {
-        alert("Error: Unable to find the output image element.");
-      }
-    };
-    img.onerror = function () {
-      alert("Error: Failed to load the image file.");
-    };
-    img.src = event.target.result;
-  };
-  reader.onerror = function () {
-    alert("Error: Failed to read the image file.");
-  };
-  reader.readAsDataURL(file);
-}
-
-function downloadImage() {
-  const outputImage = document.querySelector("#outputImage");
-  if (!outputImage) {
-    return;
-  }
-
-  const link = document.createElement("a");
-  link.href = outputImage.src;
-  link.download = "converted_image.jpg";
-  link.click();
-}
