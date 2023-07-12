59deeddc5775a8c63a103cb9e311e306adf2cc48
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-11 20:30:42 -0400

    work in progress

--- public/js/script.js
+++ public/js/script.js
@@ -1,46 +1,43 @@
-window.addEventListener("DOMContentLoaded", function () {
+import * as FilePond from 'filepond';
+
+document.addEventListener("DOMContentLoaded", function () {
   // Initialize FilePond instance
   const imageFileInput = document.querySelector("#imageFile");
-  const pond = FilePond.create(imageFileInput);
+  FilePond.create(imageFileInput);
 
-  // Get other elements
   const convertBtn = document.querySelector("#convertBtn");
   const formatSelect = document.querySelector("#formatSelect");
 
-  // Convert button click event handler
   convertBtn.addEventListener("click", function () {
-    const files = pond.getFiles();
-    if (files.length === 0) {
+    const file = imageFileInput.files[0];
+    if (!file) {
       alert("Please select an image file.");
       return;
     }
 
     const format = formatSelect.value;
 
-    files.forEach((file) => {
-      const reader = new FileReader();
-      reader.onload = function (event) {
-        const img = new Image();
-        img.onload = function () {
-          const canvas = document.createElement("canvas");
-          canvas.width = img.width;
-          canvas.height = img.height;
-          const context = canvas.getContext("2d");
-          context.drawImage(img, 0, 0);
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      const img = new Image();
+      img.onload = function () {
+        const canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const context = canvas.getContext("2d");
+        context.drawImage(img, 0, 0);
 
-          const convertedImageData = canvas.toDataURL(`image/${format}`);
-          const outputImage = document.querySelector("#outputImage");
-          if (outputImage) {
-            outputImage.src = convertedImageData;
-          }
-        };
-        img.src = event.target.result;
+        const convertedImageData = canvas.toDataURL(`image/${format}`);
+        const outputImage = document.querySelector("#outputImage");
+        if (outputImage) {
+          outputImage.src = convertedImageData;
+        }
       };
-      reader.readAsDataURL(file.file);
-    });
+      img.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
   });
 
-  // Download button click event handler
   const downloadBtn = document.querySelector("#downloadBtn");
   if (downloadBtn) {
     downloadBtn.addEventListener("click", function () {
